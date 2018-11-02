ef55b91d3d21cf62d7f133572aaa6ecff4a62701
Eterniitys <johan.guerrero394@gmail.com>
2018-11-02 20:17:07 +0000

    Fichier build/ et src/ ajouté

--- Parseur/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /win/Users/LoïcetMarie/Documents/Marie/Cours/Fac/L2/S3/Programmation C/projet/projet_c/src
-# Build directory: /win/Users/LoïcetMarie/Documents/Marie/Cours/Fac/L2/S3/Programmation C/projet/projet_c/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test-parseur "./test-parseur")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /disk/win/Mes Dossiers/Cours/Licence/S3/projects/projet_c/src
+# Build directory: /disk/win/Mes Dossiers/Cours/Licence/S3/projects/projet_c/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test-parseur "./test-parseur")
