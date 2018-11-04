b47e827a3a1a035554e2c0058f472290b2f8bfec
clmlmb <lombardini.clement@gmail.com>
2018-11-04 18:48:37 +0100

    Contient la structure d un mot, et trois prototypes pour des fonctions syllabes, phonetique, et rimes

---
+++ word.h
@@ -0,0 +1,16 @@
+#ifndef MOT_H_
+#define MOT_H_
+
+typedef struct _Mot Mot;
+
+struct _Mot {
+  char* mot;
+  char* syllabes;
+  char* phonetique;    
+};
+
+extern Mot Mot_syllabes(char Mot a);
+extern Mot Mot_phonetique(char Mot b);
+extern Mot Mot_rime(char Mot c);
+
+#endif
