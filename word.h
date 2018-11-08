8d5eea7ca5964a18d26d08b469888ec6664c4871
clmlmb <44257302+clmlmb@users.noreply.github.com>
2018-11-08 09:30:32 +0000

    Update word.h

--- word.h
+++ word.h
@@ -4,13 +4,14 @@
 typedef struct _Mot Mot;
 
 struct _Mot {
+  
   char* mot;
   char** syllabes;
   char** phonetique;
  
 };
 
-extern Mot Mot (char* mot, char** syllabes, char** phonetique);
+extern Mot creer_Mot (char* mot, char** syllabes, char** phonetique);
 extern Mot Mot_syllabes(char Mot a);
 extern Mot Mot_phonetique(char Mot b);
 extern Mot Mot_rime(char Mot c);
