b96f4bdd17f2858abedcf8a8432c4381ca81a114
cafehaine <kilian.guillaume@gmail.com>
2018-11-08 09:11:10 +0100

    Parser: reindent code

--- parseur/src/parseur.c
+++ parseur/src/parseur.c
@@ -23,15 +23,15 @@ long size_file(FILE * fichier){
 }
 
 void afficherFichier(char * nom){
-	
-	
+
+
 	// fichier = POINTEUR DE FICHIER QUI IDENTIFIE LE FLUX
 	FILE * fichier;
-	
+
 	// OUVERTURE DU FLUX 
 	fichier=fopen(nom,"r");
-	
-	
+
+
 	long sizeFichier;
 
 
@@ -40,87 +40,87 @@ void afficherFichier(char * nom){
 	}
 	else {
 		printf("le fichier existe\n");
-		
+
 		// APPEL A LA FONCTION TAILLE
 		sizeFichier=size_file(fichier);
 
-		
-		
+
+
 		//AFFICHAGE
 		printf("%ld bytes = %ld Mio\n",sizeFichier,sizeFichier/1024/1024);
-		
-		
-  		
-       char buffer[sizeFichier]; // tableau qui contiendra tout le texte 
-  
-       fseek(fichier, 0, SEEK_SET); // placement du curseur/pointeur au debut
-       
-       // on lit le fichier Lexique avec le flux fichier, 
-       //on indique le nombre de caractere avec la taille du fichier => 1 caractere = 1 octet 
-       // on place tout ca dans le buffer       
-       fread(buffer,sizeFichier,1,fichier); 
-       
 
-        
-        // ici on print le fichier dans le terminal 
-        // TODO : stocker les mots et leur forme dans la structure de mot 
-        // TODO : a capella	: mot akapEla : prononciation	a ca-pel-la : syllabe	a-ka-pE-la : phonetique
-        // TODO : syllabe et phonetique sont a stocké dans des char ** pour recuperer les syllables : [[a][ca][pel][la]] / [[a][ka][pE][la]] 
-        
-        /* FORME de la structure : 
-        	char * mot;
-       		char * prononciation;
-       		char ** syllabe;
-       		char ** phonetique;
-       	*/
-        
-        int i=0;
-        // forme= premier element de la strucuture : mot 
-        
-        // on parcours tout le fichier
-       	 while (i!=sizeFichier-1){
-       	 
-       	 	// change pas de mot 
-       	 	if (buffer[i] != '\n'){
-       	 	
-       	 		// change de forme de mot
-       	 		if (buffer[i]=='\t'){
-       	 		
-       	 			// si forme==mot -> forme = prononciation
-      				// si forme==prononciation -> forme = syllabe
-      				// si forme == syllabe -> forme = phonetique
-      				
-      				printf("%cform suivante : ",buffer[i]);
-      				// valeur a stocké dans le forme adequat de la structure
 
-     			 }
-     			// on est sur la meme forme de mot
-       	 		else {	
-       	 			if(i==0){
-		   	 			printf("Premiere forme : %c", buffer[i]);
+
+		char buffer[sizeFichier]; // tableau qui contiendra tout le texte 
+
+		fseek(fichier, 0, SEEK_SET); // placement du curseur/pointeur au debut
+
+		// on lit le fichier Lexique avec le flux fichier, 
+		//on indique le nombre de caractere avec la taille du fichier => 1 caractere = 1 octet 
+		// on place tout ca dans le buffer       
+		fread(buffer,sizeFichier,1,fichier); 
+
+
+
+		// ici on print le fichier dans le terminal 
+		// TODO : stocker les mots et leur forme dans la structure de mot 
+		// TODO : a capella	: mot akapEla : prononciation	a ca-pel-la : syllabe	a-ka-pE-la : phonetique
+		// TODO : syllabe et phonetique sont a stocké dans des char ** pour recuperer les syllables : [[a][ca][pel][la]] / [[a][ka][pE][la]] 
+
+		/* FORME de la structure : 
+		   char * mot;
+		   char * prononciation;
+		   char ** syllabe;
+		   char ** phonetique;
+		 */
+
+		int i=0;
+		// forme= premier element de la strucuture : mot 
+
+		// on parcours tout le fichier
+		while (i!=sizeFichier-1){
+
+			// change pas de mot 
+			if (buffer[i] != '\n'){
+
+				// change de forme de mot
+				if (buffer[i]=='\t'){
+
+					// si forme==mot -> forme = prononciation
+					// si forme==prononciation -> forme = syllabe
+					// si forme == syllabe -> forme = phonetique
+
+					printf("%cform suivante : ",buffer[i]);
+					// valeur a stocké dans le forme adequat de la structure
+
+				}
+				// on est sur la meme forme de mot
+				else {	
+					if(i==0){
+						printf("Premiere forme : %c", buffer[i]);
 						// valeur a stocké dans le forme adequat de la structure
-		   	 		}
-       	 			else {
-       	 				printf("%c", buffer[i]);  
-       	 				// valeur a stocké dans le forme adequat de la structure 			
-       	 			}
-       	 		}
-       	 	}
-       	 	// change de mot 
-       	 	else {
-       	 		printf("\nMot suivant : ");
-       	 		// valeur a stocké dans le forme adequat de la structure
-       	 	}
+					}
+					else {
+						printf("%c", buffer[i]);  
+						// valeur a stocké dans le forme adequat de la structure 			
+					}
+				}
+			}
+			// change de mot 
+			else {
+				printf("\nMot suivant : ");
+				// valeur a stocké dans le forme adequat de la structure
+			}
 
-       		
-      	 	i++;
-       		
-       	 }
-       	 printf("\n");
 
-	 	// FERMETURE  DU FLUX
+			i++;
+
+		}
+		printf("\n");
+
+		// FERMETURE  DU FLUX
 		fclose(fichier);
-	
+
 	}
 }
 
