55f39db6892ed26b659b07461f17ad9e0a45a56f
Eterniitys <johan.guerrero394@gmail.com>
2018-11-10 18:35:28 +0000

    gen_build: install libraries

--- gen_build.sh
+++ gen_build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-directories=("parseur" "rhyme" "arbre_syll" "list")
+directories=("list" "parseur" "rhyme" "arbre_syll")
 for dir in "${directories[@]}"; do
 	echo "Nettoyage du dossier build pour $dir"
 	rm -rf "$dir"/build
 	mkdir "$dir"/build
 	echo "Génération fichiers build pour $dir"
 	cd "$dir"/build/
+	cmake -DCMAKE_INSTALL_PREFIX=/tmp ../src
+	echo "Génération de la librairie $dir.so"
+	make
+	echo "Exportation de $dir.so dans /tmp"
+	make install
 	cmake -DCMAKE_BUILD_TYPE=Debug ../src
 	cd ../..
 done
