6c4b45afcd04794b9d79122f4e97c7496bbd62f4
Guerrero Johan <jguerrer@m-ulr-linuxubu.univ-lr.fr>
2018-11-15 11:06:21 +0100

    add tree function

--- tree/src/tree.c
+++ tree/src/tree.c
@@ -15,11 +15,11 @@
 #include "tree.h"
 
 /**
- * \fn Tree * newTree(void *struc)
+ * \fn Tree * tree_new_Tree(void *struc)
  *
  * \return seed - return a new pointer of tree.
  */
-Tree * newTree(void *struc){
+Tree * tree_new_Tree(void *struc){
 	Tree *seed = malloc(sizeof(Tree));
 	seed->struc = struc;
 	seed->children = list_new();
@@ -27,11 +27,11 @@ Tree * newTree(void *struc){
 }
 
 /**
- * \fn Tree * createTree()
+ * \fn Tree * tree_create()
  *
  * \return seed - return a new pointer of tree.
  */
-Tree * createTree(){
+Tree * tree_create(){
 	Tree *seed = malloc(sizeof(Tree));
 	seed->struc = seed;
 	seed->children = list_new();
@@ -39,29 +39,34 @@ Tree * createTree(){
 }
 
 /**
- * \fn void * getNode(Tree *root)
+ * \fn void * tree_get_node(Tree *root)
  *
  * \return root->struc - return the node.
  */
-void * getNode(Tree *root){
+void * tree_get_node(Tree *root){
 	return root->struc;
 }
 
 /**
-*\fn Tree * addBranch(Tree *root, Tree *branch)
+*\fn Tree * tree_add_branch(Tree *root, Tree *branch)
 *
 * Allows the addition of the child 'branch' to the 'root'
 */
-Tree * addBranch(Tree *root, Tree *branch){
+Tree * tree_add_branch(Tree *root, Tree *branch){
 	list_add(root->children, branch);
 	return root;
 }
 
 /**
-*\fn Tree * getBranch(Tree *root,int nb)
+*\fn Tree * tree_get_branch(Tree *root,int nb)
 *
 * Allows the getter of the child 'branch' to the 'root'
 */
-void * getBranch(Tree *root,int nb){
+void * tree_get_branch(Tree *root,int nb){
 	return list_get(root->children,nb);
 }
+
+//TODO
+int tree_count_children();
+//TODO
+void* find_child(Tree tree);
