fe770f815fb572bfa9864b50a5f05a47b913dc28
Syfalko <flavien171@hotmail.fr>
2018-11-22 08:12:56 +0000

    partie rime

--- rhyme/src/rhyme.c
+++ rhyme/src/rhyme.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+String mots[3]={"fiskal","flat","SÂ°val"};
+String [] rhyme(String [] mots, String mot1)
+{
+  for (int i = 0; i < sizeof(mots); i++)
+  {
+    String mot2=mots[i];
+    /* code */
+  }
+  bool voyelle(char lettre)
+  {
+    char table[6]={"a","e","i","o","u","y"};
+    for (int i = 0;i < sizeof(table);i++)
+    {
+      if (lettre==table[i])
+        return true;
+      else
+        return false;
+    }
+  }
 
+}
