534b7e261f947d8de5bdd71ca08881f1828fd960
cafehaine <kilian.guillaume@gmail.com>
2018-11-25 18:54:54 +0100

    Hash: add function signatures

--- hash/src/hash.h
+++ hash/src/hash.h
@@ -8,7 +8,26 @@
  */
 #ifndef _HASH_
 #define _HASH_
- 
+
+typedef struct _KeyValuePair KeyValuePair;
+
+struct _KeyValuePair {
+	char* _key;
+	void* _value;
+};
+
+typedef struct _Hashmap Hashmap;
+
+struct _Hashmap {
+	KeyValuePair** _tables;
+	char _table_count;
+};
+
+extern Hashmap* hashmap_new(void);
+extern void* hashmap_get(Hashmap* map, char* key);
+extern void hashmap_set(Hashmap* map, char* key, void* value);
+extern void hashmap_destroy(Hashmap* map);
+
 extern int hash (char * string);
 
 #endif /* _HASH_ */
