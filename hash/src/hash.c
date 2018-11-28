b0c0a29ee45392be188c8d8310055505ff7b234a
Eterniitys <johan.guerrero394@gmail.com>
2018-11-28 21:18:52 +0000

    refresh hash's breath

--- hash/src/hash.c
+++ hash/src/hash.c
@@ -19,7 +19,22 @@ int xor(int a, int b){
 	return a^b;
 }
 
-int hash (const char * string){
+void rpl(Hashmap* map){
+	int som=0;
+	for (int i = 0; i<map->_table_count;i++){
+		int inc=0;
+		for (int j = 0; j<256;j++){
+			if (map->_tables[i][j]._key != NULL){
+				inc++;
+			}
+		}
+		printf("\nTable %d remplie a %4.1f%\n",i,((float)inc/256)*100);
+		som+=inc;
+	}
+	printf("<==> Tables remplie à %4.1f% <==>\n\n",((float)som/(256*map->_table_count))*100);
+}
+
+unsigned char hash (const char * string){
 	int val[] = {19,46,7,123};
 	int i;
 	for (i=0;i<strlen(string);i++){
@@ -33,38 +48,50 @@ int hash (const char * string){
 	
 }
 
+void _new_tab(Hashmap* map){
+	map->_table_count++;
+	char nbTable = map->_table_count;
+	map->_tables = realloc(map->_tables,sizeof(KeyValuePair*)*nbTable);
+	map->_tables[nbTable-1] = malloc(sizeof(KeyValuePair)*256);
+	for (int i = 0; i<256;i++){
+		map->_tables[nbTable-1][i]._key=NULL;
+	}
+}
+
 Hashmap* hashmap_new(void){
 	Hashmap* newhash = malloc(sizeof(Hashmap));
-	newhash->_table_count=1;	
-	//First hashMap
-	KeyValuePair **tab = malloc(sizeof(KeyValuePair*)*10);
-	tab[0] = malloc(sizeof(KeyValuePair)*256);
-	for (int i = 0; i<256;i++){
-		tab[0][i]._key=NULL;
-	}
-	newhash->_tables = tab;
+	newhash->_table_count=0;
+	_new_tab(newhash);
 	return newhash;
 }
+
+
 
 void hashmap_set(Hashmap* map, char* key, void* value){
 	int nbTable = 0;
 	int place = false;
+	//printf("-Hash::%d\n",hash(key));
 	while (nbTable<map->_table_count && !place){
+		
 		if (map->_tables[nbTable][hash(key)]._key == NULL){
+			//printf("-La Place est Libre %s\n",map->_tables[nbTable][hash(key)]._key);
 			map->_tables[nbTable][hash(key)]._key = key;
 			map->_tables[nbTable][hash(key)]._value = value;
 			place = true;
-		}else if (strcmp(map->_tables[nbTable][hash(key)]._key,key)){
+		}else if (strcmp(map->_tables[nbTable][hash(key)]._key,key)==0){
+			//printf("-Contient déjà %s\n",map->_tables[nbTable][hash(key)]._key);
 			place = true;
 		}else{
+			//printf("-La Place est prise par %s\n",map->_tables[nbTable][hash(key)]._key);
 			nbTable++;
 		}
 	}
 	if(nbTable>=map->_table_count){
-		map->_tables[nbTable] = malloc(sizeof(KeyValuePair)*256);
+		rpl(map);//TODO
+		printf("\n Nouvelle Table !! - %d -\n\n",nbTable);
+		_new_tab(map);
 		map->_tables[nbTable][hash(key)]._key = key;
 		map->_tables[nbTable][hash(key)]._value = value;
-		map->_table_count++;
 	}
 }
 
--- hash/src/test-hashmap_new.c
+++ hash/src/test-hashmap_new.c
@@ -14,15 +14,22 @@
 
 #include "hash.h"
 
- 
+void _new_tab(Hashmap* map);
+
 int main(void){
 	
+	
 	Hashmap* map = hashmap_new();
-	
-	assert(sizeof(map)==sizeof(Hashmap));
-	assert(sizeof(map->_table_count) == sizeof(char));
-	assert(sizeof(map->_tables[0]) == sizeof(KeyValuePair*));
-	
+	assert(sizeof(*map)==sizeof(Hashmap));
+
+	for (int i = 0; i<126;i++){
+		assert(map->_table_count == i+1);
+		for (int j = 0; j<256;j++){
+			assert(sizeof(map->_tables[i][j]) == sizeof(KeyValuePair));
+			assert(map->_tables[i][j]._key == NULL);
+		}
+		_new_tab(map);
+	}
 	hashmap_destroy(map);
 		
 	return 0;
