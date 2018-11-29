ecd4db625ab6dfa3e9096b581f9bea95cd89bea1
LoicLagrolle <44373662+LoicLagrolle@users.noreply.github.com>
2018-11-29 11:03:55 +0100

    update word.c documentation

--- word/src/word.c
+++ word/src/word.c
@@ -1,14 +1,29 @@
+/**
+ * \file word.c
+ * \class word
+ * \author Clément & Loïc
+ * \version 0.1
+ * \date 29 november 2018
+ *
+ * Allows the creation of a word with the structure of 'word'
+ *
+ */
+
 #include <stdlib.h>
 #include <string.h>
 
 #include "word.h"
 
 ///////////////////////////////////CONSTRUCTEUR////////////////////////////////////////////
-
-Word* word_new (char* string, char** syllables, char** phonetics) //Constructeur
+/**
+ * \fn Word* word_new (char* string, char** syllables, char** phonetics)
+ *
+ * \return word_fill(word, string , syllables, phonetics) - return the word 
+ */
+Word* word_new (char* string, char** syllables, char** phonetics) 
 {
 
-	Word* word = malloc(sizeof(Word));//boîte pour la struture. Alloue de l'espace pour créer insérer le word dedans.
+	Word* word = malloc(sizeof(Word));
 
 	if (word) 
 	{
@@ -19,30 +34,48 @@ Word* word_new (char* string, char** syllables, char** phonetics) //Constructeur
 
 }
 
-Word* word_fill(Word* word, char* string, char** syllables, char** phonetics) //Remplit la boîte pour la structure, c'est ça qui créer effectivement le word.
+/**
+ * \fn Word* word_fill(Word* word, char* string, char** syllables, char** phonetics)
+ *
+ * \return word - return the word fill
+ */
+Word* word_fill(Word* word, char* string, char** syllables, char** phonetics) 
 {
-	word->string = string; //le premier word avant "->" représente le word créer dans word_create ( ou plutôt la boîte pour la structure )
-	word->syllables = syllables;//Ce qui vient après "->" c'est les paramètres de Word_create, et on dit ensuite au programme avec le "=" : " les paramètre de word sont égaux à ...( les paramètre de _fill )
+	word->string = string; 
+	word->syllables = syllables;
 	word->phonetics = phonetics;
 	return word;
 
 }
 
 //////////////////////////////////////////SETTER////////////////////////////////////////
-
+/**
+ * \fn Word* word_fill(Word* word, char* string, char** syllables, char** phonetics)
+ *
+ * \return word - return the word fill
+ */
 void word_set_string ( Word* wordToModify, char* string ) {
 	
         wordToModify->string = malloc(sizeof(char)*(strlen(string)+1));
 	strcpy(wordToModify->string,string);
 }
 
-
+/**
+ * \fn Word* word_fill(Word* word, char* string, char** syllables, char** phonetics)
+ *
+ * \return word - return the word fill
+ */
 void word_set_syllables ( Word* syllablesToModify, char** syllables ) {
 
 	syllablesToModify->syllables = syllables;
 
 }
 
+/**
+ * \fn Word* word_fill(Word* word, char* string, char** syllables, char** phonetics)
+ *
+ * \return word - return the word fill
+ */
 void word_set_phonetics ( Word* phoneticsToModify, char** phonetics ) {
 	
 	phoneticsToModify->phonetics  = phonetics;
@@ -50,19 +83,33 @@ void word_set_phonetics ( Word* phoneticsToModify, char** phonetics ) {
 }
 
 //////////////////////////////////////////GETTER/////////////////////////////////////////
-
+/**
+ * \fn char* word_get_string (Word* wordToGet)
+ *
+ * \return wordToGet->string - return the the string of the word
+ */
 char* word_get_string (Word* wordToGet) {
 
 return wordToGet->string;
 
 }
 
+/**
+ * \fn char** word_get_syllables (Word* syllablesToGet)
+ *
+ * \return syllablesToGet->syllables - return the syllables of the word
+ */
 char** word_get_syllables (Word* syllablesToGet) {
 
 return syllablesToGet->syllables;
 
 }
 
+/**
+ * \fn char** word_get_phonetics (Word* phoneticsToGet)
+ *
+ * \return phoneticsToGet->phonetics - return the phonetics of the word
+ */
 char** word_get_phonetics (Word* phoneticsToGet) {
 
 return phoneticsToGet->phonetics;
@@ -70,7 +117,11 @@ return phoneticsToGet->phonetics;
 }
 
 //////////////////////////////////////////FREE/////////////////////////////////////////
-
+/**
+ * \fn void word_free(Word* wordToFree)
+ *
+ * \return nothing - just free the allocation of the word
+ */
 void word_free(Word* wordToFree){
 	int i=0;
 	while(wordToFree->syllables[i]){
